222e823d2ad0098fc99d16f237b3dfa6ed9996ab
lalitsaini85 <lalitsaini100220000@gmail.com>
2026-03-18 17:25:05 +0530

    Added spring boot framework infra.

---
+++ SeatLockSystem/src/main/java/com/lalit/SeatLockSystem/SeatLockSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lalit.SeatLockSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatLockSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatLockSystemApplication.class, args);
+	}
+
+}
---
+++ SeatLockSystem/src/test/java/com/lalit/SeatLockSystem/SeatLockSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lalit.SeatLockSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatLockSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
